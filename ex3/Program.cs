cf468154c21a0b7fe268e3bc233b0cb433e0ba7d
Alla <bakrlona@mail.ru>
2022-12-18 19:40:38 +0500

    3 ex

---
+++ ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿ Console.WriteLine ("Введите число: ");
+      int n = Convert.ToInt32(Console.ReadLine());
+
+      if (n % 2 == 0)
+      {
+      Console.WriteLine ($"Четное число {n}");
+      }
+      else 
+      {
+        Console.WriteLine ($"Нечетное число {n}");
+      }
+      
