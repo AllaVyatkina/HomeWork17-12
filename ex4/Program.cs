5f29656faec80793bfeba8c669f26758d3d22eb5
Alla <bakrlona@mail.ru>
2022-12-18 19:42:46 +0500

    4 ex

---
+++ ex4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите число: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int index = 1;
+    
+    while (index <= n) 
+    {
+      if (index % 2 == 0) {
+        Console.WriteLine(index);
+      }
+      index += 1;
+    }
