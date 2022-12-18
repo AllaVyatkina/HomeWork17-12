649a5107a0c2540a153d14f3178785179e482e8d
Alla <bakrlona@mail.ru>
2022-12-18 18:23:02 +0500

    первая задача

---
+++ ex1/Program.cs
@@ -0,0 +1,10 @@
+﻿  Console.WriteLine("Введите первое число: ");
+    int firstNumber = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число: ");
+    int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+    if (firstNumber > secondNumber) {
+      Console.WriteLine($"max = {firstNumber}, min = {secondNumber}");
+    } else {
+      Console.WriteLine($"max = {secondNumber}, min = {firstNumber}");
+    }
